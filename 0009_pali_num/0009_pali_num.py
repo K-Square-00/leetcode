4a771bccf839610e9f47772159af568b17843ea8
Kankan <kan.kan.zhou@outlook.com>
2021-10-10 22:44:15 +0800

    20211212

---
+++ 0009_pali_num/0009_pali_num.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        return str(x) == str(x)[::-1]
+
+print(Solution().isPalindrome(12321))
